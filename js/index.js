d46b83d3dfd4a2dd4d7e17051f26013a079871aa
jaswinderwadali <jaswinder.wadali@gmail.com>
2015-11-14 13:55:25 +0530

    adding

--- js/index.js
+++ js/index.js
@@ -4,4 +4,18 @@ $(function() {
 function onDeviceReady() {
     alert('in onDeviceReady');
     console.log('in onDeviceReady');
+    $.ajax({
+        type: 'GET',
+        url: 'http://52.26.182.191:9000/scrap/blog/recent?page=1&size=10',
+        dataType: 'json',
+        success: function(log_data, status) {
+            alert('success');
+            console.log('GET log_data',log_data);
+            console.log('GET status',status);
+        },
+        error: function(e) {
+            console.log(e);
+            return false;
+        }
+    });
 }
